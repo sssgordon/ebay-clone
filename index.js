65cc8d150fbae0591e116a7c0be031a883c2ecac
sssgordon <gso7@uwo.ca>
2019-11-13 10:46:28 +0100

    Created data model for product

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const Product = require("./product/model");
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port :${port}!`));
---
+++ product/model.js
@@ -0,0 +1,30 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+// const User = require('../user/model.js')
+
+const Product = db.define(
+  "product",
+  {
+    title: {
+      type: Sequelize.STRING,
+      field: "title"
+    },
+    description: {
+      type: Sequelize.STRING,
+      field: "description"
+    },
+    picture: {
+      type: Sequelize.STRING,
+      field: "picture_url"
+    },
+    price: {
+      type: Sequelize.FLOAT,
+      field: "price"
+    }
+  },
+  { tableName: "products" }
+);
+
+// Product.belongsTo(User)
+
+module.exports = Product;
